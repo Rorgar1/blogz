87042666626ad7fb5e81755ab1f71e70484cf23f
Robin Bailey <rorgar1@yahoo.com>
2019-07-10 18:34:12 -0500

    rebuild project fourth time

--- main.py
+++ main.py
@@ -27,43 +27,54 @@ def index():
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
     blog_id = request.args.get('id')
-    title = "Build A Blog"
 
     if blog_id == None:
         posts = Blog.query.all()
         return render_template('blog.html', posts=posts, title='Build-a-blog')
     else:
         post = Blog.query.get(blog_id)
-        return render_template('blog.html', post=post, title='Blog Entry')   
+        return render_template('entry.html', post=post, title='Blog Entry')   
 
 @app.route('/newpost', methods=['POST', 'GET'])
-def newpost():
-    if request.method == 'GET':
-        return render_template('newpost.html', title="New Blog Entry")
+def new_post():
 
     if request.method == 'POST':
-        blog_title = request.form['title']
-        blog_body = request.form['body']
-        new_blog = Blog(blog_title, blog_body)
+        blog_title = request.form['blog-title']
+        blog_body = request.form['blog-entry']
 
         title_error = ''
         body_error = ''
 
-        if len(blog_title) == 0:
-            title_error = "Please enter a title."
-        if len(blog_body) == 0:
-            body_error = "Pleae create an entry."
+        if not blog_title:
+            title_error = "Please enter a blog title"
+        if not blog_body:
+            body_error = "Please enter a blog entry"
 
-        if not title_error and not body_error:
-            db.session.add(new_blog)
-            db.session.commit()
-            return redirect('/blog?id={}'.format(new_blog.id))
-        
+        if not body_error and not title_error:
+            new_entry = Blog(blog_title, blog_body)     
+            db.session.add(new_entry)
+            db.session.commit()        
+            return redirect('/blog?id={}'.format(new_entry.id)) 
         else:
-            blogs = Blog.query.all()
-            return render_template('newpost.html', title="Build a Blog!", blogs=blogs,
-                blog_title=blog_title, title_error=title_error, 
-                blog_body=blog_body, body_error=body_error)
+            return render_template('newpost.html', title='New Entry', title_error=title_error, body_error=body_error, 
+                blog_title=blog_title, blog_body=blog_body)
+    
+    return render_template('newpost.html', title='New Entry')
+       # if len(blog_title) == 0:
+       #     title_error = "Please enter a title."
+       # if len(blog_body) == 0:
+      #      body_error = "Pleae create an entry."
+
+      #  if not title_error and not body_error:
+       #     db.session.add(new_blog)
+       #     db.session.commit()
+       #     return redirect('/blog?id={}'.format(new_blog.id))
+        
+       # else:
+        #    blogs = Blog.query.all()
+        #    return render_template('newpost.html', title="Build a Blog!", blogs=blogs,
+        #        blog_title=blog_title, title_error=title_error, 
+        #        blog_body=blog_body, body_error=body_error)
 
 
 
