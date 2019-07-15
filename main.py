a8c04a3cc2d8b21f3b208eb743d2b2ff44e9dd2b
Robin Bailey <rorgar1@yahoo.com>
2019-07-15 15:42:46 -0500

    login and out complete

--- main.py
+++ main.py
@@ -8,30 +8,43 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'abcde12345' #required for session
 
 
 #creates a Blog class with title and body
 class Blog(db.Model):
 
-    owner_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     blog_body = db.Column(db.Text)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, blog_body):
+    def __init__(self, title, blog_body, owner):
         self.title = title
         self.blog_body = blog_body
+        self.owner = owner
 
 #creates a User class with email and password 
 class User(db.Model):
 
-    owner_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
    
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+#requires user to log in
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        redirect('/login')
+
 
 #login handler
 @app.route('/login', methods=['POST','GET'])
@@ -40,14 +53,25 @@ def login():
         username = request.form['username']
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
-        if user and user.pasword == password:
-            #To do: remember user has logged in
-            return redirect('/')
+        everyone = User.query.all()
+        
+        
+        if user and user.password == password:
+            session['username'] = username
+            #above line remembers the user is logged in
+            flash("Logged In")
+            return redirect('/newpost')
         else:
-            #add explanation why login failes
-            return '<h1>Error!</h1>'
+            if username not in everyone:
+                flash('Invalid username', 'error')
+            else:
+                flash('Invalid password', 'error')
+
 
     return render_template('login.html')
+
+
+
 
 #signup handler
 @app.route('/signup', methods=['POST','GET'])
@@ -61,10 +85,10 @@ def signup():
 
         existing_user = User.query.filter_by(username=username).first()
         if not existing_user:
-            new_user = User(username,password)
+            new_user = User(username, password)
             db.session.add(new_user)
             db.session.commit()
-            # TODO - "remember" the user
+            session['username'] = username #rememers username is logged in
             return redirect('/')
         else:
             # TODO - user better response messaging
@@ -75,10 +99,19 @@ def signup():
     return render_template('signup.html')
 
 
+#logout route and function
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/blog')
+
+
 #redirect to display all the blog posts
 @app.route('/')
 def index():
     return redirect('/blog')
+
+
 
 
 #display all the blog posts
@@ -92,25 +125,34 @@ def blog():
     else:
         post = Blog.query.get(blog_id)
         return render_template('entry.html', post=post, title='Blog Entry')   
-        
+
+
+
+
 #route to add a new post
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
 
+   
+
+
+
     if request.method == 'POST':
         blog_title = request.form['blog_title']
         blog_body = request.form['blog_body']
-       
+        owner = User.query.filter_by(username=session['username']).first()
         title_error = ''
         body_error = ''
+
 #error messages for blank fields
         if not blog_title:
             title_error = "Please enter a blog title"
         if not blog_body:
             body_error = "Cannot leave body blank"
+
 #add entry when all fields are filled in
         if not body_error and not title_error:
-            new_entry = Blog(blog_title, blog_body)           
+            new_entry = Blog(blog_title, blog_body, owner)           
             db.session.add(new_entry)
             db.session.commit()        
             return redirect('/blog?id={}'.format(new_entry.id)) 
