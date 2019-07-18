874da2aafec55dfaf20ceff56ce3909b71d2ee08
Robin Bailey <rorgar1@yahoo.com>
2019-07-17 19:25:54 -0500

    hashing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 import cgi 
+from hashutils import make_pw_hash, check_pw_hash
 
 
 #setting up sql 
@@ -35,14 +36,14 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
+    pw_hash = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='owner')
 
    
 
     def __init__(self, username, password):
         self.username = username
-        self.password = password
+        self.pw_hash = make_pw_hash(password)
 
     def __repr__(self):
         return '<User %r>' % self.username #defines the string representation
@@ -54,16 +55,7 @@ def require_login():
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
-#def logged_in_user():
-    #owner = User.query.filter_by(username=session['user']).first()
-    #return owner
 
-#endpoints_without_login = ['login', 'signup', 'blog', 'index']
-
-#@app.before_request
-#def require_login():
-    #if not ('user' in session or request.endpoint in endpoints_without_login):
-       # return redirect("/register")
 
 
 #login handler
@@ -76,7 +68,7 @@ def login():
         
         user = User.query.filter_by(username=username).first()
         
-        if user and user.password == password:                      #conditional breaks if user == None
+        if user and check_pw_hash(password, user.pw_hash):                      #conditional breaks if user == None
             session['username'] = username
             flash('Logged in')
             return redirect('/newpost')
